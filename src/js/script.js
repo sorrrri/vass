1439da9b5e7f85378f5d44da88d326c9d4917944
coralcat <sori.coralcat@gmail.com>
2020-07-20 09:55:48 +0900

    접종률 페이지 레이아웃 잡기2

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,26 @@
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+})
+
+
+document.documentElement.addEventListener('touchstart', function (event) {
+    if (event.touches.length > 1) {
+        event.preventDefault();
+    }
+}, false);
+
+
+
+window.addEventListener('resize', function(event){
+    location.reload()
+});
+
+
+
 const aside = document.querySelector('aside')
 const asideMenu = document.querySelector('.ico-aside-menu')
 
